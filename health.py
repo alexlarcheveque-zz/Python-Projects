1642bd3c44262b54fb85455b07e2dd5a35f963b9
Alexander L <larchevequealex@gmail.com>
2020-02-17 14:46:46 -0600

    Simple hello world and user inputs.

--- health.py
+++ health.py
@@ -9,3 +9,4 @@ potion_health = int(random.randint(25, 50) / difficulty)
 health += potion_health
 
 print(health)
+ 
---
+++ hello_you.py
@@ -0,0 +1,23 @@
+#Ask user for name
+
+name = input("What is your name?: ")
+
+#Ask users for age
+
+age = input("What is your age?: ")
+
+#Ask users for city
+
+city = input("What is your city?: ")
+
+#What they enjoy
+
+hobbies = input("What do you enjoy?: ")
+
+#Create output text
+
+output = name + age + city + hobbies
+
+#Print output to screen
+
+print(output)
