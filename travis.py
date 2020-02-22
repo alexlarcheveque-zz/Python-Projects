8801848aeec741601a5e33ebdf61d1d9136dbb67
alexlarcheveque <larchevequealex@gmail.com>
2020-02-22 10:34:17 -0600

    Added more functionality

--- travis.py
+++ travis.py
@@ -1,7 +1,5 @@
 known_users = ["Alex", "Peter", "Cat", "Emma", "George", "Harry"]
 
-print(len(known_users))
-
 while True:
     print("Hi! My name is Travis")
     name = input("What is your name?: ").strip().capitalize()
@@ -12,9 +10,17 @@ while True:
 
         if(remove == "y"):
             known_users.remove(name)
+        elif(remove == "n"):
+            print("No problem, you are still in the system.")
 
 
     else:
-        print("Name NOT recognized...")
+        print("Hmm I don't think I recognize you, {}".format(name))
+        add_me = input("Would you like to be added to the list (y/n)?: ").strip().lower()
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":
+            print("No worries, see you later")
+
 
     
