d32e33d39bef7ebbf8619adbcfce9599cc762b98
alexlarcheveque <larchevequealex@gmail.com>
2020-02-22 13:51:04 -0600

    Learned tuples, dictionaries, and others

---
+++ students.py
@@ -0,0 +1,10 @@
+students = {
+    "Alice": {"id":"ID001", "age":26, "grade":"A"},
+    "Bob":{"id":"ID002", "age":26, "grade":"B"},
+    "Claire":{"id":"ID003", "age":31, "grade":"C"},
+    "Alex":{"id":"ID004", "age":22, "grade":"D"},
+    "Sarah":{"id":"ID005", "age":19, "grade":"F"}
+    }
+
+print(students["Sarah"]["age"])
+print(students["Alex"]["id"], students["Bob"]["grade"])
