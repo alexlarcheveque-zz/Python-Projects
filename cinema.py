a77e76306ddad0b87acb2c3d9df72cc290419f95
alexlarcheveque <larchevequealex@gmail.com>
2020-02-23 12:52:09 -0600

    Added basic cinema problem

---
+++ cinema.py
@@ -0,0 +1,28 @@
+films =  {
+    "Finding Dory": [3, 5],
+    "Bourne": [18, 5],
+    "Tarzan": [15, 5],
+    "Ghost Busters": [12, 5]
+    }
+
+while True:
+    choice = input("What film do you want to watch?: ").strip().title()
+
+    if choice in films:
+        age = int(input("How old are you?: ").strip())
+
+        if age >= films[choice][0]:
+
+            if films[choice][1] > 0:
+                print("Enjoy the film!")
+                films[choice][1] -= 1
+            else:
+                print("I'm sorry we are all sold out!")
+                
+        else:
+            print("Sorry, you are too young to see that film!")
+        
+    else:
+        print("We don't have that film, sorry!")
+        
+    
